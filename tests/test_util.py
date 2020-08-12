c0f36aa16053c65887c3727a90afec109a6488cf
Martin Zibricky <mzibrick@redhat.com>
2020-08-12 17:22:04 +0200

    Add log parser tool with tests

---
+++ tests/test_util.py
@@ -0,0 +1,152 @@
+import io
+import pytest
+from util import run, match_timestamp, match_ipv4, match_ipv6
+
+LOG_LONG = 'tests/log_example_long'
+LOG_SHORT = 'tests/log_example_short'
+
+OUT_FIRST_5 = """
+Aug  9 06:51:10 user systemd[1]: Started NTP client/server.
+Aug  9 06:51:10 user audit[1]: SERVICE_START pid=1 uid=0 auid=4294967295 ses=4294967295 subj=system_u:system_r:init_t:s0 msg='unit=chronyd comm="systemd" exe="/usr/lib/systemd/systemd" hostname=? addr=? terminal=? res=success'
+Aug  9 06:51:10 user sssd[1044]: Starting up
+Aug  9 06:51:10 user systemd[1]: Starting Authorization Manager...
+Aug  9 06:51:10 user systemd[1]: Starting Hostname Service...
+""".strip()
+
+OUT_FIRST_4 = """
+Aug  9 06:51:10 user systemd[1]: Started NTP client/server.
+Aug  9 06:51:10 user audit[1]: SERVICE_START pid=1 uid=0 auid=4294967295 ses=4294967295 subj=system_u:system_r:init_t:s0 msg='unit=chronyd comm="systemd" exe="/usr/lib/systemd/systemd" hostname=? addr=? terminal=? res=success'
+Aug  9 06:51:10 user sssd[1044]: Starting up
+Aug  9 06:51:10 user systemd[1]: Starting Authorization Manager...
+""".strip()
+
+OUT_LAST_5 = """
+Aug 12 08:42:06 user kernel: audit: type=1106 audit(1597214526.043:744): pid=1992860 uid=0 auid=1000 ses=3 subj=unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023 msg='op=PAM:session_close grantors=pam_keyinit,pam_limits,pam_keyinit,pam_limits,pam_systemd,pam_unix acct="root" exe="/usr/bin/sudo" hostname=? addr=? terminal=/dev/pts/0 res=success'
+Aug 12 08:42:06 user kernel: audit: type=1104 audit(1597214526.043:745): pid=1992860 uid=0 auid=1000 ses=3 subj=unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023 msg='op=PAM:setcred grantors=pam_localuser,pam_unix acct="root" exe="/usr/bin/sudo" hostname=? addr=? terminal=/dev/pts/0 res=success'
+Aug 12 08:42:34 user systemd[1]: fprintd.service: Succeeded.
+Aug 12 08:42:34 user audit[1]: SERVICE_STOP pid=1 uid=0 auid=4294967295 ses=4294967295 subj=system_u:system_r:init_t:s0 msg='unit=fprintd comm="systemd" exe="/usr/lib/systemd/systemd" hostname=? addr=? terminal=? res=success'
+Aug 12 08:42:34 user kernel: audit: type=1131 audit(1597214554.084:746): pid=1 uid=0 auid=4294967295 ses=4294967295 subj=system_u:system_r:init_t:s0 msg='unit=fprintd comm="systemd" exe="/usr/lib/systemd/systemd" hostname=? addr=? terminal=? res=success'
+""".strip()
+
+OUT_LAST_4 = """
+Aug  9 06:51:10 user systemd[1]: Started NTP client/server.
+Aug  9 06:51:10 user audit[1]: SERVICE_START pid=1 uid=0 auid=4294967295 ses=4294967295 subj=system_u:system_r:init_t:s0 msg='unit=chronyd comm="systemd" exe="/usr/lib/systemd/systemd" hostname=? addr=? terminal=? res=success'
+Aug  9 06:51:10 user sssd[1044]: Starting up
+Aug  9 06:51:10 user systemd[1]: Starting Authorization Manager...
+""".strip()
+
+STDIN_LONG_LOG = None
+STDIN_SHORT_LOG = None
+
+with io.open(LOG_LONG) as fin:
+    STDIN_LONG_LOG = fin.read()
+with io.open(LOG_SHORT) as fin:
+    STDIN_SHORT_LOG = fin.read()
+
+
+test_data_filename = [
+    # (args, expected)
+    (['-f 5', LOG_LONG], OUT_FIRST_5),
+    (['-f 5', LOG_SHORT], OUT_FIRST_4),
+    (['-l 5', LOG_LONG], OUT_LAST_5),
+    (['-l 5', LOG_SHORT], OUT_LAST_4),
+    # With timestamp
+    (['-f 5', '-t', LOG_LONG], OUT_FIRST_5),
+    (['-f 5', '-t', LOG_SHORT], OUT_FIRST_4),
+    (['-l 5', '-t', LOG_LONG], OUT_LAST_5),
+    (['-l 5', '-t', LOG_SHORT], OUT_LAST_4),
+]
+
+@pytest.mark.parametrize("args,expected", test_data_filename)
+def test_filename_input(capsys, args, expected):
+    run(args)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected
+
+
+test_data_stdin = [
+    # (args, stdin, expected)
+    (['-f 5'], io.StringIO(STDIN_LONG_LOG), OUT_FIRST_5),
+    (['-f 5'], io.StringIO(STDIN_SHORT_LOG), OUT_FIRST_4),
+    (['-l 5'], io.StringIO(STDIN_LONG_LOG), OUT_LAST_5),
+    (['-l 5'], io.StringIO(STDIN_SHORT_LOG), OUT_LAST_4),
+    # With timestamp
+    (['-f 5', '-t'], io.StringIO(STDIN_LONG_LOG), OUT_FIRST_5),
+    (['-f 5', '-t'], io.StringIO(STDIN_SHORT_LOG), OUT_FIRST_4),
+    (['-l 5', '-t'], io.StringIO(STDIN_LONG_LOG), OUT_LAST_5),
+    (['-l 5', '-t'], io.StringIO(STDIN_SHORT_LOG), OUT_LAST_4),
+]
+
+@pytest.mark.parametrize("args,stdin,expected", test_data_stdin)
+def test_stdin_input(capsys, args, stdin, expected):
+    run(args, stdin)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected
+
+
+def test_error_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        run(['-f 10', 'log_non_existing'])
+    with pytest.raises(FileNotFoundError):
+        run(['-l 10', 'log_non_existing'])
+    with pytest.raises(FileNotFoundError):
+        run(['log_non_existing'])
+
+
+test_data_timestamp = [
+    ('Aug  9 06:51:10 user systemd[1]: Started NTP client/server.', True),
+    ('Aug 11 17:59:42 user cupsd[1154]: REQUEST loSubscrip', True),
+    ('Aug 11 23:59:42 user cupsd[1154]: REQUEST loSubscrip', True),
+    ('00:00:00', True),
+    # False
+    ('', False),
+    ('foo bar \n', False),
+    ('Aug 11 38:59:42 user cupsd[1154]: REQUEST localhost - - "POST / HTTP/1.1" 200 187 Renew-Subscrip', False),
+    ('Aug 11 23:79:42 user cupsd[1154]: REQUEST localhost - - "POST / HTTP/1.1" 200 187 Renew-Subscrip', False),
+    ('Aug 11 23:59:92 user cupsd[1154]: REQUEST localhost - - "POST / HTTP/1.1" 200 187 Renew-Subscrip', False),
+]
+
+@pytest.mark.parametrize("line,expected", test_data_timestamp)
+def test_match_timestamp(line, expected):
+    assert match_timestamp(line) == expected
+
+
+test_data_ipv4 = [
+    ('Aug  9 06:51:12 user avahi-daemon[1023]: Joining mDNS multicast group on interface virbr0.IPv4 with address 192.168.122.1.', True),
+    ('Aug  9 06:51:12 user avahi-daemon[1023]: Registering new address record for 192.168.122.1 on virbr0.IPv4.', True),
+    ('Aug  9 06:51:12 user dnsmasq-dhcp[1407]: DHCP, IP range 192.168.122.2 -- 192.168.122.254, lease time 1h', True),
+    ('Aug  9 06:51:17 user avahi-daemon[1023]: Registering new address record for 192.168.1.169 on wlp2s0.IPv4.', True),
+    ('Aug  9 06:51:17 user dnsmasq[1407]: using nameserver 127.0.0.1#53', True),
+    ('Aug  9 06:51:22 user chronyd[1092]: Selected source 162.159.200.123', True),
+    ('0.0.0.0', True),
+    ('1.1.1.1', True),
+    ('47.47.47.47', True),
+    ('183.183.183.183', True),
+    ('241.241.241.241', True),
+    ('255.255.255.255', True),
+    # False
+    ('', False),
+    ('256.256.256.256', False),
+    ('356.356.556.956', False),
+    ('Aug  9 06:51:17 user dnsmasq[1407]: using nameserver #53', False),
+]
+
+@pytest.mark.parametrize("line,expected", test_data_ipv4)
+def test_match_ipv4(line, expected):
+    assert match_ipv4(line) == expected
+
+
+test_data_ipv6 = [
+    ('2001:0db8:0000:0000:0000:ff00:0042:8329', True),
+    ('0000:0000:0000:0000:0000:0000:0000:0001', True),
+    ('2001:0db8:0000:0000:0000:ff00:0042:8329', True),
+    ('2001:0DB8:0000:0000:0000:FF00:0042:8329', True),
+    ('Aug  9 06:51:17 user avahi-daemon[1023]: Registering new address record for 2001:0db8:0000:0000:0000:ff00:0042:8329 on wlp2s0.IPv6.', True),
+    # False
+    ('', False),
+    ('20R1:0db8:0000:0000:T000:ff00:0042:8329', False),
+]
+
+@pytest.mark.parametrize("line,expected", test_data_ipv6)
+def test_match_ipv6(line, expected):
+    assert match_ipv6(line) == expected
---
+++ util.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+"""
+log parsing utility v1.02: Python CLI application that will parse logs of various kinds.
+If FILE is omitted, standard input is used instead.
+If multiple options are used at once, the result is the intersection of their
+results. The result (matching lines) is printed to standard output.
+"""
+
+# TODO add setup.py
+# TODO remove examples?
+
+#Example supported usage:
+# ------------------------
+# ./util.py -h
+# <prints help>
+#
+# cat test_0.log | ./util.py --first 10
+# <prints the first 10 lines of test_0.log>
+#
+# ./util.py -l 5 test_1.log
+# <prints the last 5 lines of test_1.log>
+#
+# ./utils.py --timestamps test_2.log
+# <prints any lines from test_2.log that contain a timestamp>
+#
+# ./util.py --ipv4 test_3.log
+# <prints any lines from test_3.log that contain an IPv4 address>
+#
+# ./util.py --ipv6 test_4.log
+# <prints any lines from test_4.log that contain an IPv6 address>
+#
+# ./util.py --ipv4 --last 50 test_5.log
+# <prints any of the last 50 lines from test_5.log that contain an IPv4 address>
+
+import argparse
+import io
+import re
+import sys
+
+from file_read_backwards import FileReadBackwards
+
+
+RE_TIMESTAMP = r'(0\d|1\d|[0-2][0-3])(:[0-5]\d){2}'
+RE_IPV4 = r'((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])'
+RE_IPV6 = r'([\dA-Fa-f]{4}\:){7}[\dA-Fa-f]{4}'
+
+
+def match_timestamp(line):
+    # Return True when matches timestamp in format 'HH:MM:SS'
+    if re.findall(RE_TIMESTAMP, line):
+        return True
+    return False
+
+
+def match_ipv4(line):
+    if re.findall(RE_IPV4, line):
+        return True
+    return False
+
+
+def match_ipv6(line):
+    if re.findall(RE_IPV6, line):
+        return True
+    return False
+
+
+def apply_filters(line, filter_funcs):
+    for f in filter_funcs:
+        if f(line):
+            continue
+        else:
+            return False
+    else:
+        return True
+
+
+def run(argv=[], stdin=None):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('file', metavar='FILE', nargs='?', default=None, help='log filename')
+    parser.add_argument('-f', '--first', metavar='NUM', type=int, help='Print first NUM lines')
+    parser.add_argument('-l', '--last', metavar='NUM', type=int, help='Print last NUM lines')
+    parser.add_argument('-t', '--timestamp', action='store_true', help='Print lines that contain a timestamp in HH:MM:SS format')
+    parser.add_argument('-i', '--ipv4', action='store_true', help='Print lines that contain an IPv4 address, matching IPs are highlighted')
+    parser.add_argument('-I', '--ipv6', action='store_true', help='Print lines that contain an IPv6 address (standard notation), matching IPs are highlighted')
+    args = parser.parse_args(args=argv)
+
+    filters = []
+    if args.timestamp:
+        filters.append(match_timestamp)
+    if args.ipv4:
+        filters.append(match_ipv4)
+    if args.ipv6:
+        filters.append(match_ipv6)
+
+    if args.file:
+        if args.first:
+            with io.open(args.file) as fin:
+                for i in range(0, args.first):
+                    if fin.readable():
+                        line = fin.readline()
+                        if apply_filters(line, filters):
+                            sys.stdout.write(line)
+                    else:
+                        break
+        elif args.last:
+            buffer = []
+            # Optimization for lage files - read from the end:
+            with FileReadBackwards(args.file) as fin:
+                for i in range(0, int(args.last)):
+                    line = fin.readline()
+                    if line:
+                        buffer.append(line)
+                    else:
+                        break
+            for l in reversed(buffer):
+                if apply_filters(l, filters):
+                    sys.stdout.write(l)
+        else:
+            with open(args.file) as fin:
+                for line in fin:
+                    if apply_filters(line, filters):
+                        sys.stdout.write(line)
+
+    else:
+        # Assume log content in stdin.
+        buffer = stdin.readlines()
+        lines = []
+        if args.first:
+            lines = buffer[0:args.first]
+        elif args.last:
+            if args.last > len(buffer):
+                lines = buffer
+            else:
+                lines = buffer[len(buffer) - args.last: len(buffer)]
+        else:
+            lines = buffer
+        for l in lines:
+            if apply_filters(l, filters):
+                sys.stdout.write(l)
+
+
+if __name__ == '__main__':
+    run(sys.argv[1:], stdin=sys.stdin)
